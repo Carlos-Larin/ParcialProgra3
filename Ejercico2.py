ae43b08fc43b760d7b8060ac98b51bf4a275a9e4
Carlos-Larin <157402737+Carlos-Larin@users.noreply.github.com>
2024-08-29 12:06:18 -0300

    dos finisher

--- Ejercico2.py
+++ Ejercico2.py
@@ -1,49 +1,45 @@
-#Hola
-class Prestamo():
-    nombrePersona=""
-    Identificcion=""
-    NombreLibro=""
-    CodigoLibro=""
-    fechaPedido=""
-    FechaMAx=""
-    DiasDespues=0
-    EstadoLibro=""
-    Sancion=""
-
+class Prestamo:
+    def __init__(self):
+        self.nombrePersona = ""
+        self.Identificcion = ""
+        self.NombreLibro = ""
+        self.CodigoLibro = ""
+        self.fechaPedido = ""
+        self.FechaMAx = ""
+        self.DiasDespues = 0
+        self.EstadoLibro = ""
+        self.Sancion = ""
 
     def pedirDatos(self):
-        self.nombrePersona=input("Nombre del prestador: ")
-        self.Identificcion=input("Codigo de su tarjeta: ")
-        self.NombreLibro=input("Nombre del libro en prestacion: ")
-        self.CodigoLibro=input("El codigo del libro: ")
-        self.fechaPedido=input("Fecha que se ha solicitado: ")
-        self.FechaMAx=input("Fecha hasta que se devolvera el libro: ")
+        self.nombrePersona = input("Nombre del prestador: ")
+        self.Identificcion = input("Codigo de su tarjeta: ")
+        self.NombreLibro = input("Nombre del libro en prestacion: ")
+        self.CodigoLibro = input("El codigo del libro: ")
+        self.fechaPedido = input("Fecha que se ha solicitado: ")
+        self.FechaMAx = input("Fecha hasta que se devolvera el libro: ")
 
     def sancion(self):
-        if(self.DiasDespues<=5):
-            print("La sancion sera de 6 dias sin prestamos")
-        elif(self.DiasDespues<=10):
-            print("La sancion sera de 15 dias sin prestamos")
+        if self.DiasDespues <= 5:
+            print("La sancion sera de 6 dias sin prestamos.")
+        elif self.DiasDespues <= 10:
+            print("La sancion sera de 15 dias sin prestamos.")
         else:
-            print("Tu sansion sera de un mes por burro")
+            print("Tu sancion sera de un mes sin prestamos.")
 
     def estadoPrestamo(self):
-        self.estadoPrestamo=input("Va a prestar o devolver un libro?: (Devolver=D/Prestar=p)").lower()
-        if(self.estadoPrestamo=="p" or "prestar"):
+        self.EstadoLibro = input("Va a prestar o devolver un libro?: (Devolver=D/Prestar=P) ").lower()
+        if self.EstadoLibro == "p" or self.EstadoLibro == "prestar":
             self.pedirDatos()
-        else:
-            self.estadoPrestamo=input("se paso de la cantidad de Dias a entregar? (s/n)")
-            if(self.estadoPrestamo=="s" or "devolver"):
-                self.DiasDespues=int(input("Cuantos Dias se paso de la fecha?: "))
+        elif self.EstadoLibro == "d" or self.EstadoLibro == "devolver":
+            estado = input("¿Se pasó de la cantidad de días para entregar? (s/n) ").lower()
+            if estado == "s":
+                self.DiasDespues = int(input("¿Cuántos días se pasó de la fecha?: "))
                 self.sancion()
             else:
                 print("Gracias por ser puntual :D")
+        else:
+            print("Opción no válida.")
 
-    
-libro=Prestamo()
+
+libro = Prestamo()
 libro.estadoPrestamo()
-
-
-
-
-
